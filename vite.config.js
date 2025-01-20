736f5fe6d45b4359c500ef111b4405b0654a08be
Steveen120 <bsa.chicaiza@yavirac.edu.ec>
2025-01-19 22:51:57 -0500

    Descri

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,20 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
-const _dirname = dirname(_filename);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
+    alias: [
+      {
+        find: '@', // Tu alias
+        replacement: resolve(__dirname, './src'), 
+      },
+    ],
   },
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 5173,
-    strictPort: true,
-  },
-  build: {
-    outDir: 'dist',
-  },
-  base: '/', // Asegura que las rutas funcionen correctamente en producción
+  base: '/',
 });
